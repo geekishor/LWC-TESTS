18c0457fa84da7120eebebc8613066a732fa0322
Kishor-MAC <kishor.oli@compliancequest.com>
2023-02-19 00:19:44 +0000

    Wire Tests

---
+++ force-app/main/default/lwc/wireApexUnitTestDemo/wireApexUnitTestDemo.js
@@ -0,0 +1,20 @@
+import { LightningElement, wire } from 'lwc';
+import getAccountList from '@salesforce/apex/AccountController.getAccountList';
+
+
+export default class WireApexUnitTestDemo extends LightningElement {
+
+    accounts;
+    error;
+        
+    @wire(getAccountList)
+    wiredAccounts({ error, data }) {
+        if(data) {
+            this.accounts = data;
+            this.error = undefined;
+        } else if(error) {
+            this.error = error;
+            this.accounts = undefined;
+        }
+    }
+}
